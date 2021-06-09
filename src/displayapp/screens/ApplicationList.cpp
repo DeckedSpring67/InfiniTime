0875c191b1e7e231982b8dd10f7c0f494edb495a
Marco Martini <deckedspring@gmail.com>
2021-06-10 00:52:07 +0200

    Replaced A with a Clock Symbol for the Alarm application

--- src/displayapp/screens/ApplicationList.cpp
+++ src/displayapp/screens/ApplicationList.cpp
@@ -63,7 +63,7 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
     {Symbols::paddle, Apps::Paddle},
     {"2", Apps::Twos},
     {"M", Apps::Motion},
-    {"A", Apps::Alarm},
+    {Symbols::clock, Apps::Alarm},
     {"", Apps::None},
   }};
 
